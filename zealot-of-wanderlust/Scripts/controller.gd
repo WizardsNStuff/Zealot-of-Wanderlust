6f303de5f78bbe2a64cb6fe4857af0734683e84c
Ethan Palmer (ajq079) <ajq079@mail.usask.ca>
2024-10-16 17:06:48 -0600

    Setup Basic MVC Node Structure

---
+++ zealot-of-wanderlust/Scripts/controller.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name Controller
+
+@export var level : Level
+
+func _physics_process(delta: float) -> void:
+	pass
---
+++ zealot-of-wanderlust/Scripts/view.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+class_name View
+
+@export var controller : Controller
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
