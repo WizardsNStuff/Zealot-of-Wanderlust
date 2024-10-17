2aa4ba93707fbe8045d95518fc317649056ae607
Torchyy <102316343+Torchyy@users.noreply.github.com>
2024-10-16 18:13:40 -0600

    Controller script started
    Changed Level to Model and changed the input buttons to use wasd instead of arrow keys.

--- zealot-of-wanderlust/Scripts/controller.gd
+++ zealot-of-wanderlust/Scripts/controller.gd
@@ -1,7 +1,14 @@
 extends Node
 class_name Controller
 
-@export var level : Level
+@export var model : Model
 
-func _physics_process(delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	# For each enemy: move them accordingly
+	for enemy in model.enemies:
+		# update enemy.velocity before move_and_slide()
+		enemy.move_and_slide()
+	
+	# Player Movement
+	model.player.velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	model.player.move_and_slide()
---
+++ zealot-of-wanderlust/Scripts/model.gd
@@ -0,0 +1,8 @@
+extends Node2D
+class_name Model
+
+# Player variable
+@export var player : CharacterBody2D
+
+# Enemies
+@export var enemies : Array[CharacterBody2D]
