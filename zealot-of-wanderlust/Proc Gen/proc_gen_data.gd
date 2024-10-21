77f484407d95b82c50b72c9eeb1ab6e6fa9c808c
Ethan Palmer (ajq079) <ajq079@mail.usask.ca>
2024-10-21 09:23:47 -0600

    added model data for room first generation

--- zealot-of-wanderlust/Proc Gen/proc_gen_data.gd
+++ zealot-of-wanderlust/Proc Gen/proc_gen_data.gd
@@ -13,7 +13,15 @@ class_name ProcGenData
 
 @export var corridor_amount : int = 5
 
-@export var room_amount_percent : float = 0.5
+@export_range(0.1, 1.0) var room_amount_percent : float = 0.5
+
+@export var min_room_width : int = 4
+
+@export var min_woom_height : int = 4
+
+@export_range(0, 10) var room_offset : int = 1
+
+@export var random_walk_room : bool = false
 
 var start_position : Vector2i = Vector2i.ZERO
 var direction_list : Array = [Vector2i.UP, Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT]
