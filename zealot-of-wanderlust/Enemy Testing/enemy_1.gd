5bde1d194d6895e5a019cf13468edab5dcc2888b
adambpp <adambpp@gmail.com>
2024-11-05 23:09:23 -0600

    removed the detection area cause it's not rlly needed for our game

--- zealot-of-wanderlust/Enemy Testing/enemy_1.gd
+++ zealot-of-wanderlust/Enemy Testing/enemy_1.gd
@@ -1,8 +1,10 @@
 extends GameCharacter
 
-var speed = 50
+var speed = 75
 var player_chase = false
-var player = null
+
+# the file path for this will obv change once we get everything hooked up to MVC
+@onready var player = $"../CharacterBody2D"
 
 func _ready() -> void:
 	### Declaring attributes from GameCharacterScript ###
@@ -10,22 +12,6 @@ func _ready() -> void:
 	defense = 20
 	main_damage = 15
 	main_damage_cooldown = 1.5
-	
-	### Connecting signals ###
-	var detection_area = $"Detection Area"
-	detection_area.connect("body_entered", Callable(self, "_on_detection_area_body_entered"))
-	detection_area.connect("body_exited", Callable(self, "_on_detection_area_body_exited"))
 
 func _physics_process(delta: float) -> void:
-	if player_chase:
-		position += (player.position - position) / speed
-
-
-func _on_detection_area_body_entered(body: Node2D) -> void:
-	player = body
-	player_chase = true
-
-
-func _on_detection_area_body_exited(body: Node2D) -> void:
-	player = null
-	player_chase = false
+	position += (player.position - position) / speed
