0f9f7ea9b96670bdf9ce23a7a1960e039d6c338d
Ethan Palmer <ethan7palmer@gmail.com>
2024-11-20 10:37:15 -0600

    cleaned up project structure and organization

--- zealot-of-wanderlust/Scripts/proc_gen_data.gd
+++ zealot-of-wanderlust/Scripts/proc_gen_data.gd
@@ -16,26 +16,26 @@ class_name ProcGenData
 @export var doors_root_node : Node
 
 # number of iterations to run the random walk aglorithm
-@export var room_iterations : int = 10
+@export var room_iterations : int = 500
 
 # length of each random walk step when generating rooms
-@export var room_walk_length : int = 10
+@export var room_walk_length : int = 20
 
 # whether to start the random walk at a new random position for each iteration
 # when true, this creates larger rooms
-@export var room_start_randomly_each_iteration : bool = true
+@export var room_start_randomly_each_iteration : bool = false
 
 # minimum allowed width for rooms
-@export var min_room_width : int = 4
+@export var min_room_width : int = 20
 
 # minimum allowed height for rooms
-@export var min_room_height : int = 4
+@export var min_room_height : int = 20
 
 # total width of the dungeon
-@export var dungeon_width : int = 20
+@export var dungeon_width : int = 80
 
 # total height of the dungeon
-@export var dungeon_height : int = 20
+@export var dungeon_height : int = 80
 
 # 
 @export var bsp_random_splits : bool = false
