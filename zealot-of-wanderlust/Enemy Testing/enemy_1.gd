3cc804c7aa662f5281af3bd0e5809d25a40fc9c7
adambpp <adambpp@gmail.com>
2024-11-22 01:15:30 -0600

    skeletons have a healthbar now, also adjusted health & dmg cooldown of skeleton

--- zealot-of-wanderlust/Enemy Testing/enemy_1.gd
+++ zealot-of-wanderlust/Enemy Testing/enemy_1.gd
@@ -11,26 +11,42 @@ var score = 10
 
 func _ready() -> void:
 	### Declaring attributes from GameCharacterScript ###
-	health = 100
+	health = 50
 	defense = 20
 	main_damage = 15
 	main_damage_cooldown = 1.5
-	$Timer.connect("timeout", Callable(self, "damaged_sprite_timer_timeout"))
+	$HealthBar.init_health(health)
+	$DamagedSpriteTimer.connect("timeout", Callable(self, "damaged_sprite_timer_timeout"))
+	$HealthBarTimer.connect("timeout", Callable(self, "health_bar_timer_timeout"))
+
+func _physics_process(delta: float) -> void:
+	# move towards player with normalized direction
+	var direction_to_player = (player.position - self.position).normalized()
+	
+	self.velocity = direction_to_player * speed
+	
+	move_and_slide()
 
 func take_damage(damage_amount : float) -> void:
 	health -= damage_amount
+	
+	# show/hide certain nodes when damage is taken
+	$HealthBar.show()
+	$HealthBarTimer.start()
+	$HealthBar._set_health(health)
 	$MainSprite.hide()
 	$DamagedSprite.show()
-	$Timer.start()
+	$DamagedSpriteTimer.start()
+	
+	# a timer could be added here to let enemy turn red and show healthbar
+	# one last time before queue_free'ing, lmk if you want that to happen
 	if health <= 0:
-		$MainSprite.hide()
-		$DamagedSprite.show()
-		$Timer.start()
 		controller.update_score(score)
 		if controller.check_key_status():
 			controller.give_player_key()
 		self.queue_free()
 
+# damage timer cooldown
 func startCooldown(delta: float) -> void:
 	if cooldown > 0:
 		cooldown -= delta
@@ -42,15 +58,9 @@ func canEnemyHit() -> bool:
 	else:
 		return false
 
-
-func _physics_process(delta: float) -> void:
-	# move towards player with normalized direction
-	var direction_to_player = (player.position - self.position).normalized()
-	
-	self.velocity = direction_to_player * speed
-	
-	move_and_slide()
-
 func damaged_sprite_timer_timeout():
 	$MainSprite.show()
 	$DamagedSprite.hide()
+
+func health_bar_timer_timeout():
+	$HealthBar.hide()
