c4aff3215abd651c4e7871d0cd1a2ae5a1839385
Torchyy <102316343+Torchyy@users.noreply.github.com>
2024-11-26 16:02:51 -0600

    Added ProjectileSizeUp Skill
    Lowered Projectile size  to compensate

---
+++ zealot-of-wanderlust/Skills/ProjectileSizeUp.gd
@@ -0,0 +1,11 @@
+extends Skill
+class_name ProjectileSizeUp
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	skill_name = "Projectile Size Up"
+	description = "Projectiles Become Larger"
+
+
+func add_effect(projectile : Projectile) -> void:
+	projectile.scale += Vector2(0.05, 0.05)
