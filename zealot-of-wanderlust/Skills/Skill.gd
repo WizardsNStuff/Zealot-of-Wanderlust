7d42740828be77a8c92eb12b5fcdbf8fb5fcfc5a
Torchyy <102316343+Torchyy@users.noreply.github.com>
2024-11-27 22:46:12 -0600

    Added New Game Logo

--- zealot-of-wanderlust/Skills/Skill.gd
+++ zealot-of-wanderlust/Skills/Skill.gd
@@ -3,7 +3,7 @@ class_name Skill
 
 var skill_name : String = "Skill"
 var description : String = "Skill Description"
-var image_reference : Resource = load("res://icon.svg")
+var image_reference : Resource = load("res://ZoW_Logo.png")
 var player : Player
 enum Rarity {COMMON, UNCOMMON, RARE, ULTRA_RARE, LEGENDARY}
 var tier : Rarity = Rarity.COMMON
